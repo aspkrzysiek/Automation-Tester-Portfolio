29df99a0ee97e75945bac218663a23441afce886
Krzysztof Rusinowicz <aspkrzysiek@gmail.com>
2026-01-01 18:40:19 +0100

    first steps

--- Playwright/tests/firstTest.spec.ts
+++ Playwright/tests/firstTest.spec.ts
@@ -2,14 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test.describe('1st test suite', () => {
 
-    test('1st test subtest', async ({ page }) => {
-        await page.goto('https://playwright.dev/');
-        await expect(page).toHaveTitle(/Playwright/); // optional assertion
-    });
-
-    test('2nd test subtest', async ({ page }) => {
-        await page.goto('...');
-        await expect(page).toHaveTitle(/.../); // optional assertion
+    test('login to saucedemo', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        await expect(page).toHaveTitle("Swag Labs");
+        await page.getByPlaceholder('Username').fill('standard_user');
+        await page.getByPlaceholder('Passowrd').fill('secret_sauce');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     });
 
 });
